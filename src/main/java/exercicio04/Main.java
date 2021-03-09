8610a092d1eb273dfb0a3a727cedaaf30ff75fcc
Matuk <mateusmatuk@fai-mg.br>
2021-03-09 20:35:47 -0300

    exercicio04

---
+++ src/main/java/exercicio04/Main.java
@@ -0,0 +1,30 @@
+package exercicio04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+	}
+
+	private void start() {
+		 
+		 int i = 0;
+		 
+		 while(i < 20) {
+			 System.out.println("Valor i: "+ i);
+			 i +=1;
+		 }
+		 int k = 0;
+		 do {
+			 System.out.println("Valor k: "+ k);
+			 k += 1;	 
+		 } while ( k < 20);
+		 System.out.println("-------------");		
+		 
+	}
+		 
+		 
+		}
+
+
